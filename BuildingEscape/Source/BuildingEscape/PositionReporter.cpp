dc422e3cb42b8224916194ad7419bf09aaa5826c
Cadtamas <cadtamas@gmail.com>
2017-11-27 21:31:13 +0100

    2017_11_27
    Position report log

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -9,8 +9,6 @@ UPositionReporter::UPositionReporter()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -19,7 +17,7 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair!"));
 	
 }
 
