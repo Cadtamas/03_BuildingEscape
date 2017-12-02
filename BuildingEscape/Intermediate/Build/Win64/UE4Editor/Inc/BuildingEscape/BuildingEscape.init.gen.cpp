0942ef162cdc0724681d010d6b1f4e8878fa2870
Cadtamas <cadtamas@gmail.com>
2017-12-02 21:13:17 +0100

    2017_12_02
    Pressure plate init

--- BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
+++ BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
@@ -20,7 +20,7 @@ void EmptyLinkFunctionForGeneratedCodeBuildingEscape_init() {}
 			ReturnPackage = CastChecked<UPackage>(StaticFindObjectFast(UPackage::StaticClass(), nullptr, FName(TEXT("/Script/BuildingEscape")), false, false));
 			ReturnPackage->SetPackageFlags(PKG_CompiledIn | 0x00000000);
 			FGuid Guid;
-			Guid.A = 0xD34BFBFC;
+			Guid.A = 0x37B3C971;
 			Guid.B = 0x9688FAC1;
 			Guid.C = 0x00000000;
 			Guid.D = 0x00000000;
