d6a8697daf320e8aac41b652dd546fe35d649012
Cadtamas <cadtamas@gmail.com>
2017-12-13 17:27:15 +0100

    2017_12_03
    Seems be ok. (Except the grabbing function)

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -8,40 +8,35 @@
 
 #define OUT
 
+
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
 }
 
+
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-
 	Owner = GetOwner();
 }
 
 void UOpenDoor::OpenDoor()
 {
-
-	//Set the door rotation
 	Owner->SetActorRotation(FRotator(0.f, OpenAngle, 0.f));
-
 }
 
 void UOpenDoor::CloseDoor()
 {
-
-	//Set the door rotation
 	Owner->SetActorRotation(FRotator(0.f, 0.f, 0.f));
-
 }
-
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -49,13 +44,14 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// Poll the Trigger Volume
-	if (GetTotalMassOfActorsOnPlate()>30.f) //TODO make into a parameter
+	if (GetTotalMassOfActorsOnPlate() > 30.f) // TODO make into a parameter
 	{
 		OpenDoor();
-		LastDoorOpenTime= GetWorld()->GetTimeSeconds();
+		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
 	}
-	//Check if it's time to close the door
-	if (GetWorld()->GetTimeSeconds()-LastDoorOpenTime>DoorCloseDelay)
+
+	// Check if it's time to close the door
+	if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay)
 	{
 		CloseDoor();
 	}
@@ -65,15 +61,16 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate()
 {
 	float TotalMass = 0.f;
 
-	//Find all overlapping actors
+	// Find all the overlapping actors
 	TArray<AActor*> OverlappingActors;
 	PressurePlate->GetOverlappingActors(OUT OverlappingActors);
-	//Iterate trough them adding their masses
-	for (const auto* Actor : OverlappingActors)
+
+	// Iterate through them adding their masses
+	for (const auto& Actor : OverlappingActors)
 	{
-		TotalMass+=Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
-		UE_LOG(LogTemp,Warning, TEXT("°s on pressure plate"), *Actor->GetName )
+		TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+		UE_LOG(LogTemp, Warning, TEXT("%s on pressure plate"), *Actor->GetName())
 	}
+
 	return TotalMass;
 }
-
